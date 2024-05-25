d2d16e62265123fdb1416e8cd4639c752f136916
sbs24043 <160653561+sbs24043@users.noreply.github.com>
2024-05-25 13:45:38 +0100

    created dashboard

--- ca2-sem1/utils.py
+++ ca2-sem1/utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from os import listdir
+from prophet.serialize import model_from_json
+
 
 # Returns a cleaned up dataframe with columns dropped and column names converted into 'variable name' like format
 def do_basic_cleanup(df, drop_cols):
@@ -26,6 +28,20 @@ def get_df_from_dir(dir_name, cols_to_drop=[]):
 def filter_df(df, item, metric, area, year=0):
     return df[(df.value > 0.0) & (df.item == item) & (df.element == metric) & (df.area == area) & (df.year >= year)]
 
-  
+
+def load_in_prophet_models(dir_name):
+   models = {}
+
+   for file in listdir(dir_name):
+      try:
+         with open(f"{dir_name}/" + file, 'r') as mf:
+             m = model_from_json(mf.read())  # Load model
+             models[file.split('.')[0]] = m
+      except Exception as e:
+          print(e)
+   return models
+
 # CONSTS
 PERCENTILE_LABELS = ["0-25%", "25-50%", "50-75%", "75-100%"]
+
+TOP_IRISH_CROPS_PRODUCTION= ['Barley', 'Beer of barley, malted', 'Oats', 'Potatoes', 'Raw cane or beet sugar (centrifugal only)', 'Sugar beet', 'Wheat']
